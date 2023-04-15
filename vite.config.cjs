a690032a40d7a73247d20077edfe5b5fa1d803cb
Santiago <santiagato>
2023-04-14 22:43:11 -0300

    configurado el proceso

--- vite.config.cjs
+++ vite.config.cjs
@@ -1,17 +1,11 @@
 import { defineConfig } from "vite";
 const { resolve } = require('path')
-//Add env variable to check if is production or development
-
 const env = process.env;
-//Set a base route if deploying on github pages
 env.mode = env.mode || "production";
-
-console.log('Environment: ', env.mode);
-
 export default defineConfig({
   root: "./",
-  //Add base route if deployiing on production
-  base: env.mode === "production" ? "/eventos/" : "/",
+  //:home: En esta linea se cambia la frase /eventos-app/ por el nombre de su repositorio
+  base: env.mode === "production" ? "/Evento/" : "/",
   build: {
     outDir: "docs",
     assetsDir: "assets",
