14899593593f6f2e628c6cda0c39d55456bb823d
santiagato <santiagato@users.noreply.github.com>
2023-05-13 00:33:16 +0000

    deploy: 224004cba3ee3eb98197918fccb5614783ff56bc

---
+++ assets/artistas-57edd2e0.js
@@ -0,0 +1,3 @@
+import"./carousel-f03b278b.js";import{R as s,_ as r,f as o,d as e}from"./firebase-bb022c0b.js";const i=s(r(e,"artistas")),n=await o(i);let l=document.getElementById("artistas");n.forEach(a=>{let t=a.data();l.innerHTML+=`<li>${t.nombres} - ${t.pais}
+        <img src="${t.foto}" alt="${t.nombres}" class="w-48 h-48 rounded-full">
+    </li>`});
