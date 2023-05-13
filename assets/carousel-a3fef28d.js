024d5b882c08af24aef58ce54267b2da85af4dbe
santiagato <santiagato@users.noreply.github.com>
2023-05-13 00:02:27 +0000

    deploy: 31fdaab12d43c813983e61ce744aacf1fd145ff9

--- assets/carousel-a3fef28d.js
+++ assets/carousel-a3fef28d.js
@@ -75,7 +75,7 @@
       d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z" />
   </svg>
 </div>
-</header>`;let gr=document.getElementById("carousel");gr.innerHTML=`<div class="relative h-56 overflow-hidden md:h-96">
+</header>`;let gr=document.getElementById("carousel");gr.innerHTML=`
 
 <div id="indicators-carousel" class="relative " data-carousel="slide">
 <!-- Carousel wrapper -->
